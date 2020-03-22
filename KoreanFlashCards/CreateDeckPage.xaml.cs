ed5ab8a0475f5224c6a4d9234e5c9149238810e8
Cornelius Nelson <corneliusnelson@Corneliuss-MacBook-Air.local>
2020-03-22 01:02:16 -0700

    Added a create page for deck

---
+++ KoreanFlashCards/CreateDeckPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace KoreanFlashCards
+{
+    public partial class CreateDeckPage : ContentView
+    {
+        public CreateDeckPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
